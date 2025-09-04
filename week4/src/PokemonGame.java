4fab21934089433f98823a14b045ee4e8003e37f
dohye0508 <aaddaamm0615@gmail.com>
2025-09-04 20:55:34 +0900

    gugudan

--- week4/src/PokemonGame.java
+++ week4/src/PokemonGame.java
@@ -5,31 +5,28 @@ class Pokemon {
 }
 
 public class PokemonGame {
-    public static void main(String[] args) {
-        Pokemon pikachu = new Pokemon();
-        Pokemon squirtle = new Pokemon();
-
-        Scanner scanner = new Scanner(System.in);
-
-        // 피카츄는 입력
-        System.out.print("피카츄의 체력을 입력하세요: ");
-        pikachu.hp = scanner.nextInt();
-
-        // 꼬부기는 고정
-        squirtle.hp = 110;
-
-        // 출력
-        if (pikachu.hp > squirtle.hp) {
-            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 여유. " +
-                    "꼬부기의 체력(" + squirtle.hp + ")보다 높아요.");
-        } else if (pikachu.hp < squirtle.hp) {
-            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 부족. " +
-                    "꼬부기의 체력(" + squirtle.hp + ")보다 낮아요.");
-        } else {
-            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 꼬부기의 체력(" +
-                    squirtle.hp + ")과 같아요.");
-        }
-
-        scanner.close();
-    }
+//    public static void main(String[] args) {
+//        Pokemon pikachu = new Pokemon();
+//        Pokemon squirtle = new Pokemon();
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("피카츄의 체력을 입력하세요: ");
+//        pikachu.hp = scanner.nextInt();
+//
+//        squirtle.hp = 110;
+//
+//        if (pikachu.hp > squirtle.hp) {
+//            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 여유. " +
+//                    "꼬부기의 체력(" + squirtle.hp + ")보다 높아요.");
+//        } else if (pikachu.hp < squirtle.hp) {
+//            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 부족. " +
+//                    "꼬부기의 체력(" + squirtle.hp + ")보다 낮아요.");
+//        } else {
+//            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 꼬부기의 체력(" +
+//                    squirtle.hp + ")과 같아요.");
+//        }
+//
+//        scanner.close();
+//    }
 }
