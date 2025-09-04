34f9b73000bfab9da7e64b9c544e1109d0a35025
dohye0508 <aaddaamm0615@gmail.com>
2025-09-04 20:40:30 +0900

    1.0v

--- week4/src/PokemonGame.java
+++ week4/src/PokemonGame.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 class Pokemon {
-    // concrete class (can produce object)
     int hp;
 }
 
@@ -9,18 +8,26 @@ public class PokemonGame {
     public static void main(String[] args) {
         Pokemon pikachu = new Pokemon();
         Pokemon squirtle = new Pokemon();
+
         Scanner scanner = new Scanner(System.in);
 
+        // 피카츄는 입력
         System.out.print("피카츄의 체력을 입력하세요: ");
         pikachu.hp = scanner.nextInt();
-        System.out.print("꼬부기의 체력을 입력하세요: ");
-        squirtle.hp = scanner.nextInt();
 
-        System.out.println("피카츄 체력: " + pikachu.hp);
-        if (pikachu.hp > 100) {
-            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 여유");
+        // 꼬부기는 고정
+        squirtle.hp = 110;
+
+        // 출력
+        if (pikachu.hp > squirtle.hp) {
+            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 여유. " +
+                    "꼬부기의 체력(" + squirtle.hp + ")보다 높아요.");
+        } else if (pikachu.hp < squirtle.hp) {
+            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 부족. " +
+                    "꼬부기의 체력(" + squirtle.hp + ")보다 낮아요.");
         } else {
-            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 부족");
+            System.out.println("피카츄의 체력(" + pikachu.hp + ")이 꼬부기의 체력(" +
+                    squirtle.hp + ")과 같아요.");
         }
 
         scanner.close();
